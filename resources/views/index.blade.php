bcdd4c4287ebab69f355376208f3e821994e1a37
ringo-code <ringoy42@gmail.com>
2021-05-01 21:45:05 +0900

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Blog</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <div class="sample1">
+            <h2 class="title">Title 1</h2>
+           <p class="body">This is a sample body </p>
+        </div>
+    </body>
+</html>
