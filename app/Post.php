56fb6a56161a02e8a0c6b27978a799b7094afabb
ringo-code <ringoy42@gmail.com>
2021-05-12 20:48:08 +0900

    “[add]ブログ投稿の作成”

--- app/Post.php
+++ app/Post.php
@@ -6,8 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+
     public function getPaginate()
     {
         return $this->orderBy('updated_at','DESC')->paginate();
     }
+    
 }
